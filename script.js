7a5bb5bf8a9f0085cbddfb015e138c8386940092
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-13 12:43:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	for(let i = 0;i<s1.length;i++)
+		{
+			if (s1.slice(i,i+s2.length) == s2){
+				return i;
+			}       
+		}
+	return -1;
 }
 
 // Please do not change the code below
